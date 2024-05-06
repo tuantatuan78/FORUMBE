03b10c71ff44aefb0f18e2c5922bc61f059320a5
tuantatuan78 <test@gmail.com>
2024-05-06 12:16:12 +0700

    OK

---
+++ Forumbe-test/src/main/java/com/web/BlogbeApplication.java
@@ -0,0 +1,12 @@
+package com.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogbeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogbeApplication.class, args);
+    }
+
+}
